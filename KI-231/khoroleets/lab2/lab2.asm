f0b12a4901f1d7fe8206be3fc7f95c2a67abaabd
Vladyslav <vladkhorolets4847@gmail.com>
2024-09-30 19:17:55 +0300

    Додано змінений код lab2.asm

---
+++ KI-231/khoroleets/lab2/lab2.asm
@@ -0,0 +1,39 @@
+bits 64
+
+extern ExitProcess
+extern GetStdHandle
+extern WriteConsoleA
+
+section .data
+    message db 'KI-231 Khorolets', 0Ah, 0
+
+section .bss
+    written resq 1
+
+section .text
+global main
+
+main:
+    ; Отримуємо дескриптор стандартного виводу
+    mov rcx, -11             ; STD_OUTPUT_HANDLE
+    call GetStdHandle        ; Зберігаємо дескриптор виводу
+    mov r12, rax             ; Зберігаємо результат в r12
+
+    ; Підготовка параметрів для WriteConsoleA
+    mov rcx, r12             ; hConsoleOutput
+    lea rdx, [message]       ; lpBuffer
+    mov r8, 18                ; nNumberOfCharsToWrite
+    lea r9, [written]        ; lpNumberOfCharsWritten
+    xor eax, eax             ; Обнуляємо верхню частину rax
+    push rax                 ; lpReserved (must be 0)
+    sub rsp, 32              ; Виділяємо 32 байти тіньового простору
+
+    ; Виводимо повідомлення
+    call WriteConsoleA
+
+    ; Відновлюємо стек
+    add rsp, 40              ; 32 + 8 (для lpReserved)
+
+    ; Виходимо з програми
+    xor ecx, ecx             ; exit code 0
+    call ExitProcess
