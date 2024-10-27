6379a81b96f1e869a08f17129fb5772ffc840f85
Artem Mylytsia <amylytsia@gmail.com>
2024-10-27 21:42:07 +0200

    lab5-upd

--- lib/auto.sh
+++ lib/auto.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo Detected hostname, user and IP >> your-new-file.txt && echo $HOSTNAME $HOME >> your-new-file.txt && hostname -i >> your-new-file.txt
+echo Detected hostname, user >> your-new-file.txt && echo $HOSTNAME $HOME >> your-new-file.txt
