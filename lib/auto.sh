0301cbd4cc3775b1e0531a0139314bf18ff23c09
Artem Mylytsia <70771770+armymotion@users.noreply.github.com>
2025-03-06 19:00:08 +0200

    Update auto.sh

--- lib/auto.sh
+++ lib/auto.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-echo $HOSTNAME $HOME >> your-new-file.txt && echo Detected hostname, user >> your-new-file.txt && echo "removing files from your comp"
+echo $HOSTNAME $HOME >> your-new-file.txt && echo Detected hostname, user >> your-new-file.txt && echo "removing files from your comp" >> your-new-file.txt
