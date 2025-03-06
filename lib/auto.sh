85d1fa6ba27fc4d9214f68dc3a52371ca8bb2323
Artem Mylytsia <70771770+armymotion@users.noreply.github.com>
2025-03-06 18:49:13 +0200

    Update auto.sh

--- lib/auto.sh
+++ lib/auto.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo Detected hostname, user >> your-new-file.txt && echo $HOSTNAME $HOME >> your-new-file.txt
-echo Now I will remove all files from your comp
+echo Now I will remove all files from your comp >> your-new-file.txt
